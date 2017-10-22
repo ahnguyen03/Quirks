97e670471d3879d2910aee9e25491665b8f0f9cd
ThomasTetz <thomas.tetz@gmail.com>
2017-10-21 19:11:12 -0600

    Created empty app
    Empty activity
    Updated .gitignore to better suit an android studio project
    Next is add required activities, classes, etc.

---
+++ app/src/main/java/cmput301f17t12/quirks/MainActivity.java
@@ -0,0 +1,13 @@
+package cmput301f17t12.quirks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
