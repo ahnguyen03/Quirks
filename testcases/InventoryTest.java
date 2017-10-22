8ffdef7d615c4ce41f3d8338d6f72036e6378449
charleshoang1 <choang1@ualberta.ca>
2017-10-22 11:18:10 -0600

    Created test cases for DropListTest

--- testcases/InventoryTest.java
+++ testcases/InventoryTest.java
@@ -3,28 +3,55 @@
 public class InventoryTest {
 
   // Test adding Quirk to inventory
-  public void testAddQuirk() {
-  	Quirk myQuirk = new Quirk("some_type", "some_title", 123);
+  public void testAddDrop() {
+  	Drop drop = new drop("drop", "this is drop", "Rare");
     Inventory myInventory = new Inventory();
-    myInventory.addQuirk(Quirk);
+    myInventory.addQuirk(drop);
+    ArrayList<Drop> dropList = new ArrayList<Drop>();
+    dropList.add("drop");
 
-    assertEquals(myInventory.getList()[0], myQuirk);
+    assertEquals(myInventory, dropList);
   }
 
   // Test if Quirk exists in Inventory
-  public void testHasQuirk() {
-    Event event = new Event("ate lettuce");
-    assertEquals(event.getComment(), "ate lettuce");
+  public void testHasDrop() {
+    Drop drop = new drop("drop", "this is drop", "Rare");
+    Inventory myInventory = new Inventory();
+    myInventory.addQuirk(Quirk);
+    assertTrue(myInventory.hasDrop(drop));
   }
 
    // Test to return the Quirk at location from Inventory
-  public void testGetQuirk() {
-    Event event = new Event("ate lettuce");
-    assertEquals(event.getComment(), "ate lettuce");
+  public void testGetDrop() {
+    Drop drop = new drop("drop", "this is drop", "Rare");
+    Inventory myInventory = new Inventory();
+    myInventory.addQuirk(Quirk);
+
+    assertEquals(myInventory.getDrop(0), drop);
   }
 
   // Test to remove Quirk from Inventory
-  public void testRemoveQuirk() {
-    Event event = new Event("ate lettuce");
-    assertEquals(event.getComment(), "ate lettuce");
+  public void testRemoveDrop() {
+    Drop drop = new drop("drop", "this is drop", "Rare");
+    Inventory myInventory = new Inventory();
+    myInventory.addQuirk(drop);
+    ArrayList<Drop> dropList = new ArrayList<Drop>();
+    myInventory.removeDrop(drop);
+    assertEquals(myInventory, dropList);
+  }
+
+  // Test to remove Quirk from Inventory
+  public void testGetList() {
+    Drop drop = new drop("drop", "this is drop", "Rare");
+    Drop drop1 = new drop("drop1", "this is drop1", "Common");
+
+    Inventory myInventory = new Inventory();
+    myInventory.addQuirk(drop);
+    myInventory.addQuirk(drop);
+
+    ArrayList<Drop> dropList = new ArrayList<Drop>();
+    dropList.add(drop);
+    dropList.add(drop1);
+
+    assertEquals(myInventory, dropList);
   }
